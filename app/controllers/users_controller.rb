8c3ee6b014ac453c2e90f8c46044db59a00a63e8
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-05-19 20:40:16 +0530

    Create simple POST request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  protect_from_forgery with: :null_session # avoid CSRF check
+
   def new
+  end
+
+  def create
+    render json: {mesg: "Posted successfully"}, status: :forbidden
   end
 end
